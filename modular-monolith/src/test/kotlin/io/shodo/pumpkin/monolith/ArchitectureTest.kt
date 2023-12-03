20f9d8279ef3c7fc68093aff46c08d875dbf2a03
Sylvain Decout <sylvain.decout+github@gmail.com>
2023-12-03 20:51:50 +0100

    :building_construction: Add ArchUnit rules for modular monolith

---
+++ modular-monolith/src/test/kotlin/io/shodo/pumpkin/monolith/ArchitectureTest.kt
@@ -0,0 +1,24 @@
+package io.shodo.pumpkin.monolith
+
+import com.tngtech.archunit.base.DescribedPredicate.alwaysTrue
+import com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices
+import org.junit.jupiter.api.Tag
+
+@Tag("Architecture")
+@AnalyzeClasses(packages = ["io.shodo.pumpkin.monolith"])
+class ArchitectureTest {
+
+    @ArchTest
+    val subdomains_should_not_depend_on_one_another: ArchRule = slices()
+        .matching("io.shodo.pumpkin.monolith.(*)..")
+        .namingSlices("'$1' subdomain")
+        .should().notDependOnEachOther()
+        .ignoreDependency(alwaysTrue(), resideInAPackage("io.shodo.pumpkin.monolith.shared.."))
+        .`as`("Subdomains should not depend on one another")
+        .because("they are meant to be isolated. Communication should go through proper APIs.")
+
+}

8b159aafd9f5c092a157d396ff3f6bf3f9bd55f5
Sylvain Decout <sylvain.decout+github@gmail.com>
2023-12-09 10:36:39 +0100

    :arrow_up: Bump Spring Boot from 3.1.5. to 3.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
   base
   kotlin("jvm") version "1.9.20" apply false
   kotlin("plugin.spring") version "1.9.20" apply false
-  id("org.springframework.boot") version "3.1.5" apply false
+  id("org.springframework.boot") version "3.2.0" apply false
   id("io.spring.dependency-management") version "1.1.4" apply false
   id("nu.studer.jooq") version "8.2.1" apply false
 }

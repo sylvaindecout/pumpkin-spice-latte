9e7ade2d59b37cc0e907c2efc54c3bd19e186e26
samuel <lefebvre.lsamuel@gmail.com>
2024-03-08 10:23:55 +0100

    conf cucumber add html print

--- modular-monolith/src/test/kotlin/io/shodo/pumpkin/monolith/RunCucumberTest.kt
+++ modular-monolith/src/test/kotlin/io/shodo/pumpkin/monolith/RunCucumberTest.kt
@@ -1,15 +1,17 @@
 package io.shodo.pumpkin.monolith
 
 
-import io.cucumber.junit.platform.engine.Constants
-import org.junit.platform.suite.api.*
+import io.cucumber.junit.platform.engine.Constants.*
+import org.junit.platform.suite.api.ConfigurationParameter
+import org.junit.platform.suite.api.IncludeEngines
+import org.junit.platform.suite.api.SelectClasspathResource
+import org.junit.platform.suite.api.Suite
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-    @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "io.shodo.pumpkin.monolith.stepdefs")
-    @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
-
-//@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "src/test/resources/features/rename.feature")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.shodo.pumpkin.monolith.stepdefs")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value="html:target/cucumber-reports/Cucumber.html")
 class RunCucumberTest {
 }
